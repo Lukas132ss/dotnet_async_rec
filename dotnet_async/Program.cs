5d5e0bba39812d7fa441030c5a7c3a56ec834453
Lukas de Souza Santos <lk@serrante.com.br>
2025-01-25 15:50:08 -0300

    Utilizando tasks no lugar das threads

--- dotnet_async/Program.cs
+++ dotnet_async/Program.cs
@@ -1,32 +1,20 @@
-﻿void LerArquivo()
+﻿Task<string> conteudoTask = Task.Run(() => File.ReadAllText("voos.txt"));
+
+void LerArquivo()
 {
-    var conteudo = File.ReadAllText("voos.txt");
-    Thread.Sleep(new Random().Next(300, 8000));
-    Console.WriteLine($"Conteúdo: \n{conteudo}");
+    Task.Delay(new Random().Next(300, 8000));
+    Console.WriteLine($"Conteúdo: \n{conteudoTask.Result}");
 }
 
 void ExibirRelatorio()
 {
     Console.WriteLine("Executando relatório de compra de passagens!");
-    Thread.Sleep(new Random().Next(300,8000));
+    Task.Delay(new Random().Next(300,8000));
 }
 
-var thread1 = new Thread(() => LerArquivo());
+Task task1 = Task.Run(() => LerArquivo());
 
-var thread2 = new Thread(() => ExibirRelatorio());
-
-
-void InicializarThreads()
-{
-    thread1.Start();
-    while (thread1.IsAlive)
-    {
-        Console.WriteLine("Thread 1 em execução...");
-    }
-    thread2.Start();
-}
-
-InicializarThreads();
+var task2 = Task.Run(() => ExibirRelatorio());
 
 Console.WriteLine("Outras operações.");
 Console.ReadKey();
