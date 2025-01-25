012d3e5df5e568a503200ca3255c9ad34a7ae9f9
Lukas de Souza Santos <lk@serrante.com.br>
2025-01-25 10:19:59 -0300

    Utilizando thread para executar um trecho de codigo em paralelo

--- dotnet_async/Program.cs
+++ dotnet_async/Program.cs
@@ -1,1 +1,11 @@
-﻿
+﻿void LerArquivo()
+{
+    var conteudo = File.ReadAllText("voos.txt");
+    Console.WriteLine($"Conteúdo: \n{conteudo}");
+}
+
+var thread = new Thread(() => LerArquivo());
+thread.Start();
+
+Console.WriteLine("Outras operações.");
+Console.ReadKey();
