8584f8c19361a3cc7396471e0c4b8a4f3f79b0f9
Lukas de Souza Santos <lk@serrante.com.br>
2025-03-01 08:53:16 -0300

    curso async/await

--- dotnet_async/Program.cs
+++ dotnet_async/Program.cs
@@ -1,31 +1,93 @@
-﻿Task<string> conteudoTask = Task.Run(() => File.ReadAllTextAsync("voos.txt"));
+﻿#region Programação Assíncrona
+//object chave = new object();
+//Task<string> conteudoTask;
+//lock(chave)
+//{
+//    conteudoTask = Task.Run(() => File.ReadAllTextAsync("voos.txt"));
+//}
+//async Task LerArquivoAsync(CancellationToken token)
+//{
+//    try
+//    {
+//        await Task.Delay(new Random().Next(300, 8000));
+//        token.ThrowIfCancellationRequested();
+//        Console.WriteLine($"Conteúdo: \n{conteudoTask.Result}");
 
-async void LerArquivoAsync()
+//    }
+//    catch (OperationCanceledException ex)
+//    {
+//       Console.WriteLine($"Tarefa cancelada: {ex.Message}");
+//    }
+//    catch (AggregateException ex)
+//    {
+//        Console.WriteLine($"Aconteceu o erro: {ex.InnerException.Message}");
+//    }
+
+//}
+
+//async Task ExibirRelatorioAsync(CancellationToken token)
+//{
+//	try
+//	{
+//        Console.WriteLine("Executando relatório de compra de passagens!");
+//        await Task.Delay(new Random().Next(300, 8000));
+//        token.ThrowIfCancellationRequested();
+//    }
+//	catch(OperationCanceledException ex)
+//	{
+
+//        Console.WriteLine($"Tarefa cancelada: {ex.Message}");
+//	}
+
+//}
+
+//CancellationTokenSource tokenDeCancelamento = new CancellationTokenSource();
+
+//Task tarefa= Task.WhenAll(LerArquivoAsync(tokenDeCancelamento.Token), ExibirRelatorioAsync(tokenDeCancelamento.Token));
+
+//await Task.Delay(1000).ContinueWith(_ => tokenDeCancelamento.Cancel());
+
+//Console.WriteLine("Outras operações.");
+//Console.ReadKey();
+#endregion
+
+using dotnet_async.Client;
+using dotnet_async.Modelos;
+
+var client = new JornadaMilhasClient(new JornadaMilhasClientFactory().CreateClient());
+
+async Task ProcessarConsultasDeVoosAsync()
 {
     try
     {
-        await Task.Delay(new Random().Next(300, 8000));
-        Console.WriteLine($"Conteúdo: \n{conteudoTask.Result}");
+        CancellationTokenSource tokenSource = new CancellationTokenSource();
+
+        tokenSource.Cancel();
+
+        var voos = await client.ConsultarVoosAsync(tokenSource.Token);
+
+        foreach (var voo in voos)
+        {
+            Console.WriteLine(voo);
+        }
     }
-    catch (AggregateException ex)
+    catch (Exception ex)
     {
-        Console.WriteLine($"Erro: {ex.InnerException?.Message}");
+
+        Console.WriteLine($"tarefa cancelada: {ex.Message}");
     }
     
 }
 
-async void ExibirRelatorioAsync()
+await ProcessarConsultasDeVoosAsync();
+
+async Task ComprarPassagemAsync()
 {
-    Console.WriteLine("Executando relatório de compra de passagens!");
-    await Task.Delay(new Random().Next(300,8000));
+    var compraPassagemRequest = new CompraPassagemRequest() {Origem="Vitória",Destino="Belém",Milhas =1000 };
+
+    var resultado = client.ComprarPassagemAsync(compraPassagemRequest);
+
+    Console.WriteLine(resultado.Result);
 }
 
-//Task task1 = Task.Run(() => LerArquivo());
-
-//Task task2 = Task.Run(() => ExibirRelatorio());
-
-LerArquivoAsync();
-ExibirRelatorioAsync();
-
-Console.WriteLine("Outras operações.");
-Console.ReadKey();
+await ComprarPassagemAsync();
